298c3b234a844d6e1b647f88e13b44560333a680
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-02-07 16:58:55 -0300

    Documentaçã Settings

--- APIGuia/Settings.cs
+++ APIGuia/Settings.cs
@@ -1,6 +1,8 @@
 ﻿namespace APIGuia;
 
+// Classe static para armazenar configurações globais
 public static class Settings
 {
+    // Chave secreta para assinar token jwt
     public static string secret = "aB1@dE2#fG3$hI4%jK5&lM6*nO7(pQ8)rS9+tU0-vWzYx1#W2!e3@R4$t5^u6&I7*o8(p9)Q0rA1-B2+c3D4=E5F6*G7H8";
 }
