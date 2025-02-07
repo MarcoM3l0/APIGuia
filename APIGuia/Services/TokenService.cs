f4b336b6da2deb159ac00502ee96b2a25b1c17db
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-02-07 16:56:35 -0300

    Documentação TokenService

--- APIGuia/Services/TokenService.cs
+++ APIGuia/Services/TokenService.cs
@@ -8,23 +8,31 @@ namespace APIGuia.Services;
 
 public static class TokenService
 {
+    // Método para gerar um token
     public static string GenerateToken(User user)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(Settings.secret);
+        var tokenHandler = new JwtSecurityTokenHandler(); // Manipulador de token
+        var key = Encoding.ASCII.GetBytes(Settings.secret); // Chave de segurança
 
+        // Configuração do token
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            // Define as claims do token
             Subject = new ClaimsIdentity(new[]
             {
-                new Claim(ClaimTypes.Name, user.Email),
-                new Claim(ClaimTypes.Role, user.TipoFuncionario)
+                new Claim(ClaimTypes.Name, user.Email), // Adiciona o email como claim
+                new Claim(ClaimTypes.Role, user.TipoFuncionario) // Adiciona o tipo de funcionário como claim
             }),
-            Expires = DateTime.UtcNow.AddHours(2),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            Expires = DateTime.UtcNow.AddHours(2), // Tempo de expiração do token
+            SigningCredentials = new SigningCredentials(
+                                                    new SymmetricSecurityKey(key), // Chave de segurança
+                                                    SecurityAlgorithms.HmacSha256Signature) // Algoritmo de criptografia
         };
 
+        // Criar o token com base nas informações passadas
         var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        // Retorna o token gerado como string
         return tokenHandler.WriteToken(token);
     }
-} 
+}
