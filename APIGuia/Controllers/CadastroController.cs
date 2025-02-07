2551935effa148b031396b3638d2e8d02dbddd1f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-02-07 16:21:08 -0300

    Documentação CadastroController

--- APIGuia/Controllers/CadastroController.cs
+++ APIGuia/Controllers/CadastroController.cs
@@ -14,6 +14,7 @@ public class CadastroController : ControllerBase
 {
     private readonly APIGuiaDBContext _context;
 
+    // Injeção de dependência do contexto do banco de dados
     public CadastroController(APIGuiaDBContext context)
     {
         _context = context;
@@ -23,39 +24,53 @@ public class CadastroController : ControllerBase
     [Authorize(Roles = "webmaster")]
     public async Task<ActionResult<User>> PostUser(UserDTO userDto)
     {
+
+        // Validação dos campos obrigatórios
         if (string.IsNullOrWhiteSpace(userDto.Nome) || string.IsNullOrWhiteSpace(userDto.Email) || string.IsNullOrWhiteSpace(userDto.password))
         {
             return BadRequest("Nome, Email e Senha são obrigatórios.");
         }
 
+        // Verifica se o email já está cadastrado
         if (await _context.Usuarios.AnyAsync(u => u.Email == userDto.Email))
         {
             return Conflict("Já existe um usuário com este email.");
         }
 
+
+        // Cria um novo usuário com os dados do DTO e criptografa a senha
         var user = new User
         {
             Nome = userDto.Nome,
             Email = userDto.Email.ToLower(),
-            TipoFuncionario = userDto.TipoFuncionario.ToLower(),
-            password = BCrypt.Net.BCrypt.HashPassword(userDto.password) 
+            TipoFuncionario = userDto.TipoFuncionario.ToLower(), 
+            password = BCrypt.Net.BCrypt.HashPassword(userDto.password) // Criptografa a senha
         };
 
+        // Adiciona o usuário ao banco de dados e salva as alterações
         _context.Usuarios.Add(user);
         await _context.SaveChangesAsync();
+
+        // Retorna o usuário criado com o status 201 (Created)
         return CreatedAtAction("GetUser", new { id = user.UserId }, user);
     }
 
-    [HttpGet("{id}", Name = "GetUser")]
+
+
+    // Endpoint para obter um usuário
+    [HttpGet(Name = "GetUser")]
     public async Task<ActionResult<User>> GetUser(int id)
     {
+        // Busca o usuário no banco de dados pelo ID
         var user = await _context.Usuarios.FindAsync(id);
 
+        // Retorna 404 se o usuário não for encontrado
         if (user == null)
         {
             return NotFound();
         }
 
+        // Retorna o usuário encontrado
         return Ok(new UserDTO(user));
     }
 
